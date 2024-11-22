5cdc5ec761f683dd1153d0229844a000491bcd3b
Necro-sword <gokulkumarn2k4@gmail.com>
2024-11-22 20:20:12 +0530

    first commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+
+const userAuth = async (req, res, next) => {
+    const { token } = req.headers;
+    if (!token) {
+        return res.json({ success: false, message: 'not authorized login again' })
+    }
+    try {
+        const tokenDecode = jwt.verify(token, process.env.JWT_SECRET)
+        if (tokenDecode.id) {
+            req.userId = tokenDecode.id
+        } else {
+            return res.json({ success: false, message: 'not authorized login again' })
+            next();
+        }
+    } catch (error) {
+        res.json({ success: false, message: error.message })
+    }
+}
+export default userAuth;
---
+++ server/routes/imageRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import { generateImage } from '../controllers/imageController.js'
+
+
+const imageRouter = express.Router()
+
+imageRouter.post('/generate-image',generateImage)
+
+export default imageRouter;
